42eb1cbd692941a125be25db4fa35a12fe9c0ba1
C3dvall <96820963+C3dvall@users.noreply.github.com>
2026-04-29 13:03:50 +0000

    Relabled dogs data in ESC50

--- Thesis/Labelling_Script/simple_relabel.py
+++ Thesis/Labelling_Script/simple_relabel.py
@@ -3,7 +3,7 @@ from pathlib import Path
 SCRIPT_DIR = Path(__file__).resolve().parent
 
 # Change only this folder
-BASE_PATH = SCRIPT_DIR.parent.parent / "soundfiles" / "ESC50" / "cat"
+BASE_PATH = SCRIPT_DIR.parent.parent / "soundfiles" / "ESC50" / "dog"
 
 # Automatically becomes "wind", "rain", "thunder", etc.
 LABEL_SUFFIX = BASE_PATH.name
