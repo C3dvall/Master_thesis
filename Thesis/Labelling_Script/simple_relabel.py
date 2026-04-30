a0e4624d61793e319c6a67de13dd71b3c5be56e2
C3dvall <96820963+C3dvall@users.noreply.github.com>
2026-04-30 13:31:52 +0000

    updated filename for car_horn

--- Thesis/Labelling_Script/simple_relabel.py
+++ Thesis/Labelling_Script/simple_relabel.py
@@ -3,7 +3,7 @@ from pathlib import Path
 SCRIPT_DIR = Path(__file__).resolve().parent
 
 # Change only this folder
-BASE_PATH = SCRIPT_DIR.parent.parent / "soundfiles" / "ESC50" / "laughing"
+BASE_PATH = SCRIPT_DIR.parent.parent / "soundfiles" / "ESC50" / "car_horn"
 
 # Automatically becomes "wind", "rain", "thunder", etc.
 LABEL_SUFFIX = BASE_PATH.name
